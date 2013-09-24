fe027ff64982b2f32cd0f5ef8995cbe400c4c085
Stu Rice <stuedric@gmail.com>
2013-09-24 15:28:42 +0100

    first for new cricket

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cricket::Application.config.secret_token = '9d22615e70d5e2e3235e169b9f0b30560c661f95274b6a7ba5c9a4994715fd9f8a3434c97b187b581dbf9dfd17622a57a33bf5eaad5340d6a557dc46ef67f544'
